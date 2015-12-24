4afb6bc78e2300ddbbe58051a32c3bc0317dba7f
Dave <daves125125@gmail.com>
2015-12-24 20:30:33 +0000

    Dockerization

--- src/main/java/com/djh/sampleit/metadata/DefaultMetricMetadataService.java
+++ src/main/java/com/djh/sampleit/metadata/DefaultMetricMetadataService.java
@@ -1,5 +1,8 @@
 package com.djh.sampleit.metadata;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
@@ -9,12 +12,12 @@ import java.util.Date;
  */
 public class DefaultMetricMetadataService implements MetricMetadataService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultMetricMetadataService.class);
+
     @Override
     public MetricMetadata collectMetricMetadata() {
-
         String hostname = retrieveHostname();
-        Date date = new Date();
-        return new MetricMetadata(hostname, date);
+        return new MetricMetadata(hostname, new Date());
     }
 
     private String retrieveHostname() {
@@ -25,7 +28,7 @@ public class DefaultMetricMetadataService implements MetricMetadataService {
             InetAddress inetAddress = InetAddress.getLocalHost();
             hostname = inetAddress.getHostName();
         } catch (UnknownHostException ex) {
-            System.out.println("Hostname can not be resolved");
+            LOG.info("Hostname can not be resolved");
         }
 
         return hostname;
